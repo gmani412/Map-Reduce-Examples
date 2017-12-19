3c53fd733b2e62a1a8ebbbfc72c337fb0058cda4
Manindar G <gmani412@gmail.com>
2017-12-19 17:28:07 +0530

    Updated the output key & value class details

--- MRMaxLengthWord/src/main/java/com/mani/mrmaxlengthword/MyDriver.java
+++ MRMaxLengthWord/src/main/java/com/mani/mrmaxlengthword/MyDriver.java
@@ -44,8 +44,8 @@ public class MyDriver {
          */
 //        job.setCombinerClass(MyReducer.class);
         job.setReducerClass(MyReducer.class);
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(IntWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
